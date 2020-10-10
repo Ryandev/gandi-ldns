5f47f502491355737115c006532f7e177eae2dcc
Ryan Powell <ryangarethpowell@gmail.com>
2020-10-10 12:26:43 +0100

    Added support for subdomain

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Gandi-DDNS
 Dynamic DNS tool to update your hostname record to your computers public ip address
+
 [![License](https://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://github.com/ryandev/BlueSkies/blob/master/LICENSE) [![Docker](https://img.shields.io/docker/automated/ryandev/gandi-ldns)](https://hub.docker.com/repository/docker/ryandev/gandi-ldns)
 
-## Usage
+## Usage (main record) 
+For records `NAME='@'` `TYPE='A'`
 ```
 docker run --rm -e "APIKEY=myapikey" -e "DOMAIN=mydomainname" ryandev/gandi-ldns
 ```
 where `mydomainname` is the website you have registered on gandi.net you want updated to your public ip & `myapikey` is the *production* api key (see here for more info setting it up: https://docs.gandi.net/en/domain_names/advanced_users/api.html)
-*Thats it! maybe you'll want to put the above in cron, but that's everything*
+*Thats it!*
+after running `mydomainname` will be pointing to your public ip address
+
+## Usage (subdomain)
+For records `NAME='$SUBDOMAIN'` `TYPE='A'` 
+Same as above, except specify argument `SUBDOMAIN` example:
+```
+docker run --rm -e "APIKEY=myapikey" -e SUBDOMAIN="vpn" -e "DOMAIN=ryandev.com" ryandev/gandi-ldns
+```
+This runs the same as above, except, vpn.ryandev.com will be pointing to your public address now
 
 ### About
 This project is forked from 
--- gandi-ldns.py
+++ gandi-ldns.py
@@ -24,9 +24,28 @@ def get_domain_records(domain, apiurl, apikey):
     ip = '0.0.0.0'
 
     resp = requests.get(urlfetch, headers={'X-Api-Key': apikey})
+    print(resp.json())
     resp.raise_for_status()
 
     return resp.json()
+
+def filter_domain_records(recordsIn, searchkey, searchval):
+    recordsout=[]
+    for record in recordsIn:
+        if record[searchkey] == searchval:
+            recordsout.append(record)
+    return recordsout
+
+def filter_records_with_recordtype(recordsIn, typeValue):
+    return filter_domain_records(recordsIn, 'rrset_type', typeValue)
+
+def filter_records_with_recordvalue(recordsIn, typeValue):
+    if type(typeValue) != []:
+        typeValue = [typeValue]
+    return filter_domain_records(recordsIn, 'rrset_values', typeValue)
+
+def filter_records_with_recordname(recordsIn, nameValue):
+    return filter_domain_records(recordsIn, 'rrset_name', nameValue)
 
 def update_domain_record(domain, apiurl, apikey, matchtype, matchname, value, ttl=10800):
     endpoint = 'domains/%s/records/%s/%s' % (domain, matchname, matchtype)
@@ -36,34 +55,34 @@ def update_domain_record(domain, apiurl, apikey, matchtype, matchname, value, tt
         'rrset_ttl': ttl,
         'rrset_values': [value,],
     }
-    print(body)
     resp = requests.put(puturl, json=body, headers={'X-Api-Key': apikey})
     resp.raise_for_status()
 
-def filter_domain_records(recordsIn, searchkey, searchval):
-    recordsout=[]
-    for record in recordsIn:
-        if record[searchkey] == searchval:
-            recordsout.append(record)
-    return recordsout
-
-def get_zone_ip(domain, apiurl, apikey):
+def get_ip_for_domain(domain, apiurl, apikey):
     """Get the current IP from the A record in the DNS zone """
 
     # # There may be more than one A record - we're interested in one with
     # # the specific name (typically @ but could be sub domain)
     domain_records = get_domain_records(domain, apiurl, apikey)
-    domain_records = filter_domain_records(domain_records, 'rrset_type', 'A')
-    domain_records = filter_domain_records(domain_records, 'rrset_name', '@')
+    domain_records = filter_records_with_recordtype(domain_records, 'A')
+    domain_records = filter_records_with_recordname(domain_records, '@')
     if len(domain_records) > 0:
         return domain_records[0]['rrset_values'][0]
     else:
         return None
 
-def set_zone_ip(domain, apiurl, apikey, newip, ttl):
-    """ Update Gandi record A=#newip """
+def get_ip_for_subdomain(domain, subdomain, apiurl, apikey):
+    """Get the current IP from the A record in the DNS zone """
 
-    update_domain_record(domain=domain, apiurl=apiurl, apikey=apikey, matchtype='A', matchname='@', value=newip, ttl=ttl)
+    # # There may be more than one A record - we're interested in one with
+    # # the specific name (typically @ but could be sub domain)
+    domain_records = get_domain_records(domain, apiurl, apikey)
+    domain_records = filter_records_with_recordtype(domain_records, 'A')
+    domain_records = filter_records_with_recordname(domain_records, subdomain)
+    if len(domain_records) > 0:
+        return domain_records[0]['rrset_values'][0]
+    else:
+        return None
 
 def get_public_ip():
     """ Get external IP """
@@ -78,14 +97,57 @@ def get_public_ip():
 
     return resp.text
 
+def set_ip_for_domain(domain, apiurl, apikey, newip, ttl):
+    """ Update Gandi record A=#newip """
 
-def main():
+    update_domain_record(domain=domain, apiurl=apiurl, apikey=apikey, matchtype='A', matchname='@', value=newip, ttl=ttl)
+
+def set_ip_for_subdomain(domain, subdomain, apiurl, apikey, newip, ttl):
+    """ Update Gandi record A=#newip """
+
+    update_domain_record(domain=domain, apiurl=apiurl, apikey=apikey, matchtype='A', matchname=subdomain, value=newip, ttl=ttl)
+
+def check_and_update_record_domain(domain, apiurl, apikey, ttl):
     current_ip = get_public_ip().strip()
 
+    zone_ip = get_ip_for_domain(domain=domain, apiurl=apiurl, apikey=apikey).strip()
+
+    if zone_ip == current_ip:
+        print('Record up to date, returning ('+zone_ip+')')
+    else:
+        print('Updating record to ('+zone_ip+')')
+        set_ip_for_domain(
+            domain=domain, 
+            apiurl=apiurl, 
+            apikey=apikey, 
+            newip=current_ip,
+            ttl=ttl)
+        print('DNS A record update complete - set to ', zone_ip)
+
+def check_and_update_record_subdomain(domain, subdomain, apiurl, apikey, ttl):
+    current_ip = get_public_ip().strip()
+
+    zone_ip = get_ip_for_subdomain(domain=domain, subdomain=subdomain, apiurl=apiurl, apikey=apikey).strip()
+
+    if zone_ip == current_ip:
+        print('Record up to date, returning ('+zone_ip+')')
+    else:
+        print('Updating record to ('+zone_ip+')')
+        set_ip_for_subdomain(
+            domain=domain, 
+            subdomain=subdomain,
+            apiurl=apiurl, 
+            apikey=apikey, 
+            newip=current_ip,
+            ttl=ttl)
+        print('DNS A record update complete - set to ', zone_ip)
+
+def main():
     apikey = get_env_variable('APIKEY', None)
     apiurl = get_env_variable('APIURL', 'https://dns.api.gandi.net/api/v5/')
     ttl = get_env_variable('TTL', 10800)
     domain = get_env_variable('DOMAIN', None)
+    subdomain = get_env_variable('SUBDOMAIN', None)
 
     if apikey == None:
         print('Missing \'APIKEY\' from env. See https://docs.gandi.net/en/domain_names/advanced_users/api.html')
@@ -95,19 +157,20 @@ def main():
         print('Missing \'DOMAIN\' from env. Set this to the website we\'re updating')
         sys.exit(1)
 
-    zone_ip = get_zone_ip(domain=domain, apiurl=apiurl, apikey=apikey).strip()
-
-    if zone_ip == current_ip:
-        print('Record up to date, returning ('+zone_ip+')')
+    if subdomain != None:
+        check_and_update_record_subdomain(
+            domain=domain, 
+            subdomain=subdomain, 
+            apiurl=apiurl, 
+            apikey=apikey, 
+            ttl=ttl)
     else:
-        print('Updating record to ('+zone_ip+')')
-        set_zone_ip(
+        check_and_update_record_domain(
             domain=domain, 
             apiurl=apiurl, 
             apikey=apikey, 
-            newip=current_ip,
             ttl=ttl)
-        print('DNS A record update complete - set to ', zone_ip)
+
 
 
 if __name__ == "__main__":
